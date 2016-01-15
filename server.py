b715ae989bd40f1ce5f5b1b5c36bde3763b79d20
George Stefanis <info@georgestefanis.com>
2016-01-15 17:36:03 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,60 @@
+import os
+# import json
+from flask import Flask, Response, request, jsonify
+from instagram.client import InstagramAPI
+
+app = Flask(__name__, static_url_path='', static_folder='public')
+app.config.update(
+    PROPAGATE_EXCEPTIONS=True
+)
+app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
+
+
+CONFIG = {
+    'client_id': 'ad842c2390844f0581e5b178c410741a',
+    'client_secret': '9d94784498f5489db11f2281a5248dc6',
+    'redirect_uri': 'http://46.101.29.114:7000/handle_auth'
+}
+instagram_client = InstagramAPI(**CONFIG)
+
+
+@app.route('/get_url')
+def auth_instagram():
+    try:
+        url = instagram_client.get_authorize_url(scope=["likes", "comments"])
+        return jsonify(url=url)
+    except Exception as e:
+        print(e)
+
+
+def get_nav():
+    nav_menu = ("<h1>Instagram Actions</h1>"
+                "<ul>"
+                "<li><a href='/likeHashtags'>Like a Hashtag</a></li>"
+                "<li><a href='/getPhotos'>Get Hashtag photos</a></li>"
+                "</ul>")
+    return nav_menu
+
+
+@app.route('/handle_auth')
+def on_success():
+    code = request.args.get("code")
+    if not code:
+        return "Missing code"
+    try:
+        access_token, user_info = instagram_client.exchange_code_for_access_token(code)
+        if not access_token:
+            return "could not get access token"
+        request.session['access_token'] = access_token
+    except Exception as e:
+        print(e)
+    return get_nav()
+
+
+@app.route('/api/hashtag', methods=['GET'])
+def get_hashtags():
+    return Response('Hello World!')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 7000)))
