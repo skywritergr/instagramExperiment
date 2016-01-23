45cd44f2b388956e471d6f22cdc13e69a2e39e65
George Stefanis <info@georgestefanis.com>
2016-01-22 19:41:54 -0500

    handling returned result from instagram and redirecting to the correct page after success

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import os
-# import json
-from flask import Flask, Response, request, jsonify, send_from_directory
+from flask import Flask, Response, request, jsonify, send_from_directory, redirect
 from instagram.client import InstagramAPI
 from server.instagram import instagram_user
 
@@ -11,7 +10,6 @@ app.config.update(
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
 
-
 CONFIG = {
     'client_id': 'ad842c2390844f0581e5b178c410741a',
     'client_secret': '9d94784498f5489db11f2281a5248dc6',
@@ -20,6 +18,7 @@ CONFIG = {
 instagram_client = InstagramAPI(**CONFIG)
 access_token = ""
 user = None
+
 
 @app.route('/handleauth')
 def handleauth():
@@ -55,13 +54,13 @@ def on_success():
     if not code:
         return "Missing code"
     try:
-        access_token, user_info = 
+        access_token, user_info =\
             instagram_client.exchange_code_for_access_token(code)
         if not access_token:
             return "could not get access token"
         access_token = access_token
         user = instagram_user(user_info)
-        return jsonify(access_token=access_token, user_info=user_info)
+        return redirect("http://46.101.29.114:7000/handleauth", code=302)
     except Exception as e:
         print(e)
         return jsonify(error=True)
--- server/instagram.py
+++ server/instagram.py
@@ -3,10 +3,10 @@ class instagram_user(object):
     instagram_id = "0"
     profile_picture = ""
     username = ""
-    
+
     def __init__(self, user_info):
-        print(user_info)
-        self.full_name = user_info.full_name
-        self.instagram_id = user_info.id
-        self.profile_picture = user_info.profile_picture
-        self.username = user_info.username
+        print(type(user_info))
+        self.full_name = user_info['full_name']
+        self.instagram_id = user_info['id']
+        self.profile_picture = user_info['profile_picture']
+        self.username = user_info['username']
